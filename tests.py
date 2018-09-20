5e37d8ae8eec80283425a8bc2287b1264f9d45ea
Scott Ouellette <scottx611x@gmail.com>
2018-09-20 15:33:15 -0400

    Update tests

--- tests.py
+++ tests.py
@@ -3,23 +3,43 @@ from unittest import mock
 from postgres_upgrade import RDSPostgresUpgrader
 
 
+@mock.patch("time.sleep")
 class RDSPostgresUpgraderTests(unittest.TestCase):
     def setUp(self):
+        self.pg_engine_versions = ["9.4.18", "9.5.13", "9.6.9", "10.4"]
+        self.db_instance_statuses = [
+            "backing_up", "available", "upgrading", "modifying", "available"
+        ] * len(self.pg_engine_versions)
         self.rds_pg_upgrader = RDSPostgresUpgrader(
             "test-rds-id",
-            ["test-targeted-upgrade-version"]
+            self.pg_engine_versions
         )
 
+    def test_wait_for_db_status_desired(self, sleep_mock):
+        with mock.patch(
+            "postgres_upgrade.RDSPostgresUpgrader.get_db_status",
+            return_value="available"
+        ):
+            self.rds_pg_upgrader.wait_for_db_status("available")
+
+    def test_wait_for_db_status_undesired(self, sleep_mock):
+        with mock.patch(
+            "postgres_upgrade.RDSPostgresUpgrader.get_db_status",
+            return_value="upgrading"
+        ):
+            self.rds_pg_upgrader.wait_for_db_status(
+                "available", is_undesired_status=True
+            )
+
     @mock.patch.object(RDSPostgresUpgrader, "_modify_db")
-    @mock.patch("time.sleep")
     def test_upgrade(self, sleep_mock, modify_db_mock):
         with mock.patch(
             "postgres_upgrade.RDSPostgresUpgrader.get_db_status",
-            side_effect=["backing_up", "available", "upgrading",
-                         "modifying", "available"]
-        ) as modify_db_mock:
+            side_effect=self.db_instance_statuses
+        ):
             self.rds_pg_upgrader.upgrade()
-        self.assertTrue(modify_db_mock.called)
+        self.assertEqual(modify_db_mock.call_count,
+                         len(self.pg_engine_versions) + 1)
 
 if __name__ == '__main__':
     unittest.main()
