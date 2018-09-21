ac083e60beac3190fea1c8fc0ef24559ba46bc5a
Scott Ouellette <scottx611x@gmail.com>
2018-09-21 11:40:22 -0400

    Add a test that hits the parser

--- tests.py
+++ tests.py
@@ -1,26 +1,25 @@
 import unittest
 from unittest import mock
-from pg_upgrader import RDSPostgresUpgrader
+from pg_upgrader import RDSPostgresUpgrader, create_parser
 
 
 class RDSPostgresUpgraderTests(unittest.TestCase):
-    def setUp(self):
-        self.pg_engine_versions = ["9.4.18", "9.5.13", "9.6.9", "10.4"]
-        self.rds_pg_upgrader = RDSPostgresUpgrader(
-            "test-rds-id",
-            self.pg_engine_versions
+    @mock.patch("pg_upgrader.RDSPostgresUpgrader._modify_db")
+    def test_upgrade(self, modify_db_mock):
+        pg_engine_versions = ["9.4.18", "9.5.13", "9.6.9", "10.4"]
+        RDSPostgresUpgrader("test-rds-id", pg_engine_versions).upgrade()
+        self.assertEqual(modify_db_mock.call_count, len(pg_engine_versions))
+
+    @mock.patch("pg_upgrader.RDSPostgresUpgrader._modify_db")
+    def test_upgrade_after_parse(self, modify_db_mock):
+        parser = create_parser()
+        args = parser.parse_args(
+            ['-id', 'test-rds-id', '-v', "9.4.18"]
         )
-
-    @mock.patch("pg_upgrader.RDSPostgresUpgrader._modify_db")
-    def test_upgrade_many_versions(self, modify_db_mock):
-        self.rds_pg_upgrader.upgrade()
-        self.assertEqual(modify_db_mock.call_count,
-                         len(self.pg_engine_versions))
-
-    @mock.patch("pg_upgrader.RDSPostgresUpgrader._modify_db")
-    def test_upgrade_single_version(self, modify_db_mock):
-        RDSPostgresUpgrader("test-rds-id", "9.4.18").upgrade()
+        RDSPostgresUpgrader(args.rds_db_instance_id,
+                            args.pg_target_versions).upgrade()
         self.assertEqual(modify_db_mock.call_count, 1)
+
 
 if __name__ == '__main__':
     unittest.main()
