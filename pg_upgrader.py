930f48f03f4dc2e82b6dc967cc25f051226a9c2b
Scott Ouellette <scottx611x@gmail.com>
2018-09-21 13:45:23 -0400

    Run unsupported usage check queries through refinery ec2 to access RDS instances in private subnet

--- pg_upgrader.py
+++ pg_upgrader.py
@@ -1,22 +1,40 @@
 import argparse
+import sys
 import time
 
 import boto3
+import paramiko
 
 
 class RDSPostgresUpgrader():
-    client = boto3.client('rds')
+    rds_client = boto3.client('rds')
 
-    def __init__(self, db_instance_id, pg_engine_versions):
+    def __init__(self, refinery_ec2_ip, db_instance_id, pg_engine_versions):
         self.db_instance_id = db_instance_id
         self.pg_engine_versions = pg_engine_versions
+        self.refinery_ec2_ip = refinery_ec2_ip
+
+    def _check_for_unsupported_usage(self):
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh_client.connect(self.refinery_ec2_ip, username="ubuntu",
+                           password=input("PrivateKey Password: "))
+        with open("unsupported_usage_checks.txt") as f:
+            for command in f.readlines():
+                stdin, stdout, stderr = ssh_client.exec_command(command)
+                rows_found = int(str(stdout.read()).split("\\n")[2])
+                if rows_found != 0:
+                    sys.exit(
+                        "Pre-check failed.\n Command {} yielded {} row(s)"
+                        .format(command, rows_found)
+                    )
 
     def _modify_db(self, pg_engine_version):
         print("Waiting for {} to become available".format(self.db_instance_id))
-        self.client.get_waiter("db_instance_available").wait(
+        self.rds_client.get_waiter("db_instance_available").wait(
             DBInstanceIdentifier=self.db_instance_id
         )
-        self.client.modify_db_instance(
+        self.rds_client.modify_db_instance(
             DBInstanceIdentifier=self.db_instance_id,
             EngineVersion=pg_engine_version,
             AllowMajorVersionUpgrade=True,
@@ -27,13 +45,23 @@ class RDSPostgresUpgrader():
         time.sleep(30)
 
     def upgrade(self):
-        [self._modify_db(pg_engine_version)
-            for pg_engine_version in self.pg_engine_versions]
+        for pg_engine_version in self.pg_engine_versions:
+            self._check_for_unsupported_usage()
+            self._modify_db(pg_engine_version)
 
 
 def create_parser():
     parser = argparse.ArgumentParser(
         description='Gather RDSPostgresUpgrader configurables.'
+    )
+    parser.add_argument(
+        '-ip', '--refinery_ec2_ip', type=str,
+        help=(
+            "IP of Refinery EC2 to run Postgres upgrade "
+            "compatibility queries on. Refinery's RDSInstances reside in "
+            "private subnets and can't be queried directly"
+        ),
+        required=True
     )
     parser.add_argument(
         '-id', '--rds_db_instance_id', type=str,
@@ -50,7 +78,7 @@ def create_parser():
 
 def main():
     args = create_parser().parse_args()
-    RDSPostgresUpgrader(args.rds_db_instance_id,
+    RDSPostgresUpgrader(args.refinery_ec2_ip, args.rds_db_instance_id,
                         args.pg_target_versions).upgrade()
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -4,22 +4,27 @@ from pg_upgrader import RDSPostgresUpgrader, create_parser
 
 
 class RDSPostgresUpgraderTests(unittest.TestCase):
+    @mock.patch("pg_upgrader.RDSPostgresUpgrader._check_for_unsupported_usage")
     @mock.patch("pg_upgrader.RDSPostgresUpgrader._modify_db")
-    def test_upgrade(self, modify_db_mock):
+    def test_upgrade(self, modify_db_mock, check_unsupported_mock):
         pg_engine_versions = ["9.4.18", "9.5.13", "9.6.9", "10.4"]
-        RDSPostgresUpgrader("test-rds-id", pg_engine_versions).upgrade()
+        RDSPostgresUpgrader('X.X.X.X', "test-rds-id",
+                            pg_engine_versions).upgrade()
         self.assertEqual(modify_db_mock.call_count, len(pg_engine_versions))
+        self.assertEqual(check_unsupported_mock.call_count,
+                         len(pg_engine_versions))
 
+    @mock.patch("pg_upgrader.RDSPostgresUpgrader._check_for_unsupported_usage")
     @mock.patch("pg_upgrader.RDSPostgresUpgrader._modify_db")
-    def test_upgrade_after_parse(self, modify_db_mock):
+    def test_upgrade_after_parse(self, modify_db_mock, check_unsupported_mock):
         parser = create_parser()
         args = parser.parse_args(
-            ['-id', 'test-rds-id', '-v', "9.4.18"]
+            ['-ip', 'X.X.X.X', '-id', 'test-rds-id', '-v', "9.4.18"]
         )
-        RDSPostgresUpgrader(args.rds_db_instance_id,
+        RDSPostgresUpgrader(args.refinery_ec2_ip, args.rds_db_instance_id,
                             args.pg_target_versions).upgrade()
         self.assertEqual(modify_db_mock.call_count, 1)
-
+        self.assertEqual(check_unsupported_mock.call_count, 1)
 
 if __name__ == '__main__':
     unittest.main()
