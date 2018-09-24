87539620d8a295e5ff787385fad1993fb2a4217e
Scott Ouellette <scottx611x@gmail.com>
2018-09-24 17:30:20 -0400

    Update tests

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest import mock
-from pg_upgrader import RDSPostgresUpgrader, create_parser
+from pg_upgrader import (RDSPostgresInstance, RDSPostgresUpgrader,
+                         create_parser, rds_client)
 from test_fixtures.fixtures import (describe_db_instances,
                                     list_tags_for_resource,
                                     non_postgres_describe_db_instances)
@@ -8,101 +9,94 @@ from test_fixtures.fixtures import (describe_db_instances,
 
 class RDSPostgresUpgraderTests(unittest.TestCase):
     @mock.patch.object(
-        RDSPostgresUpgrader.client, "describe_db_instances",
+        rds_client, "describe_db_instances",
         return_value=describe_db_instances
     )
-    @mock.patch.object(RDSPostgresUpgrader.client, "get_waiter")
-    @mock.patch.object(RDSPostgresUpgrader.client, "modify_db_instance")
+    @mock.patch.object(rds_client, "get_waiter")
+    @mock.patch.object(rds_client, "modify_db_instance")
     @mock.patch("time.sleep")
     def test_upgrade(self, sleep_mock, modify_db_mock, get_waiter_mock,
                      describe_dbs_mock):
         db_instance_ids = ["test-rds-id-a", "test-rds-id-b"]
-        pg_engine_versions = ["9.4.18", "9.5.13", "9.6.9", "10.4"]
-        RDSPostgresUpgrader(pg_engine_versions, ids=db_instance_ids).upgrade()
+        pg_engine_versions = ['9.4.18', '9.5.13', '9.6.9', '10.4']
+        with mock.patch.object(
+            RDSPostgresInstance, "get_engine_upgrade_path",
+            return_value=pg_engine_versions
+        ):
+            RDSPostgresUpgrader(ids=db_instance_ids).upgrade_all()
         db_modication_count = len(pg_engine_versions) * len(db_instance_ids)
         self.assertEqual(get_waiter_mock.call_count, db_modication_count * 2)
         self.assertEqual(modify_db_mock.call_count, db_modication_count)
 
     @mock.patch.object(
-        RDSPostgresUpgrader.client, "describe_db_instances",
+        rds_client, "describe_db_instances",
         return_value=describe_db_instances
     )
-    @mock.patch("pg_upgrader.RDSPostgresUpgrader._modify_db")
+    @mock.patch("pg_upgrader.RDSPostgresInstance._modify_db")
     def test_upgrade_after_parse_ids(self, modify_db_mock, describe_dbs_mock):
         parser = create_parser()
-        args = parser.parse_args(
-            ['-ids', 'test-rds-id', '-versions', "9.4.18"]
-        )
-        RDSPostgresUpgrader(args.pg_target_versions,
-                            ids=args.rds_db_instance_ids).upgrade()
+        args = parser.parse_args(['-ids', 'test-rds-id'])
+        RDSPostgresUpgrader(ids=args.rds_db_instance_ids).upgrade_all()
         self.assertEqual(modify_db_mock.call_count, 1)
 
     @mock.patch.object(
-        RDSPostgresUpgrader.client, "list_tags_for_resource",
+        rds_client, "list_tags_for_resource",
         return_value=list_tags_for_resource
     )
     @mock.patch.object(
-        RDSPostgresUpgrader.client, "describe_db_instances",
+        rds_client, "describe_db_instances",
         return_value=describe_db_instances
     )
-    @mock.patch("pg_upgrader.RDSPostgresUpgrader._modify_db")
+    @mock.patch("pg_upgrader.RDSPostgresInstance._modify_db")
     def test_upgrade_after_parse_tags(self, modify_db_mock, describe_dbs_mock,
                                       list_tags_mock):
         parser = create_parser()
         args = parser.parse_args(
-            ['-tags', '{"Name": "test-rds-name", "owner": "test@example.com"}',
-             '-versions', "9.6.9 10.4"]
+            ['-tags', '{"Name": "test-rds-name", "owner": "test@example.com"}']
         )
         rds_postgres_upgrader = RDSPostgresUpgrader(
-            args.pg_target_versions,
             tags=args.rds_db_instance_tags
         )
-        rds_postgres_upgrader.upgrade()
+        rds_postgres_upgrader.upgrade_all()
 
         self.assertEqual(describe_dbs_mock.call_count, 2)
         self.assertEqual(list_tags_mock.call_count, 1)
         self.assertEqual(modify_db_mock.call_count, 1)
-        self.assertEqual(rds_postgres_upgrader.db_instance_ids, ["rds-test"])
+        self.assertEqual(len(rds_postgres_upgrader.rds_instances), 1)
 
     @mock.patch.object(
-        RDSPostgresUpgrader.client, "list_tags_for_resource",
+        rds_client, "list_tags_for_resource",
         return_value=list_tags_for_resource
     )
     @mock.patch.object(
-        RDSPostgresUpgrader.client, "describe_db_instances",
+        rds_client, "describe_db_instances",
         return_value=describe_db_instances
     )
-    @mock.patch("pg_upgrader.RDSPostgresUpgrader._modify_db")
+    @mock.patch("pg_upgrader.RDSPostgresInstance._modify_db")
     def test_nothing_upgraded_after_parsed_tags_not_matched(
         self, modify_db_mock, describe_dbs_mock, list_tags_mock
     ):
         parser = create_parser()
         args = parser.parse_args(
-            ['-tags', '{"Name": "This Tag does not exist",'
-                ' "owner": "test@example.com"}',
-             '-versions', "9.6.9 10.4"]
+            ['-tags',
+             '{"Name": "Bad Tag Value", "owner": "test@example.com"}']
         )
-        RDSPostgresUpgrader(args.pg_target_versions,
-                            tags=args.rds_db_instance_tags).upgrade()
+        RDSPostgresUpgrader(tags=args.rds_db_instance_tags).upgrade_all()
 
         self.assertEqual(describe_dbs_mock.call_count, 1)
         self.assertEqual(list_tags_mock.call_count, 1)
         self.assertEqual(modify_db_mock.call_count, 0)
 
-    @mock.patch("pg_upgrader.RDSPostgresUpgrader._modify_db")
+    @mock.patch("pg_upgrader.RDSPostgresInstance._modify_db")
     def test_nothing_upgraded_if_postgres_engine_not_found(self,
                                                            modify_db_mock):
         parser = create_parser()
-        args = parser.parse_args(
-            ['-ids', 'test-rds-id',
-             '-versions', "9.6.9 10.4"]
-        )
+        args = parser.parse_args(['-ids', 'test-rds-id'])
         with mock.patch.object(
-            RDSPostgresUpgrader.client, "describe_db_instances",
+            rds_client, "describe_db_instances",
             return_value=non_postgres_describe_db_instances
         ) as describe_dbs_non_postgres_mock:
-            RDSPostgresUpgrader(args.pg_target_versions,
-                                ids=args.rds_db_instance_ids).upgrade()
+            RDSPostgresUpgrader(ids=args.rds_db_instance_ids).upgrade_all()
 
         self.assertEqual(describe_dbs_non_postgres_mock.call_count, 1)
         self.assertEqual(modify_db_mock.call_count, 0)
