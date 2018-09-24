739e8099ad38e19b6abfda8e2d9b952d5cb1121a
Scott Ouellette <scottx611x@gmail.com>
2018-09-24 11:15:41 -0400

    Add test coverage

--- tests.py
+++ tests.py
@@ -51,5 +51,33 @@ class RDSPostgresUpgraderTests(unittest.TestCase):
         self.assertEqual(modify_db_mock.call_count, 1)
         self.assertEqual(rds_postgres_upgrader.db_instance_ids, ["rds-test"])
 
+    @mock.patch.object(
+        RDSPostgresUpgrader.client, "list_tags_for_resource",
+        return_value=list_tags_for_resource
+    )
+    @mock.patch.object(
+        RDSPostgresUpgrader.client, "describe_db_instances",
+        return_value=describe_db_instances
+    )
+    @mock.patch("pg_upgrader.RDSPostgresUpgrader._modify_db")
+    def test_nothing_upgraded_after_parsed_tags_not_matched(
+        self, modify_db_mock, describe_dbs_mock, list_tags_mock
+    ):
+        parser = create_parser()
+        args = parser.parse_args(
+            ['-tags', '{"Name": "This Tag does not exist",'
+                ' "owner": "test@example.com"}',
+             '-versions', "9.6.9 10.4"]
+        )
+        rds_postgres_upgrader = RDSPostgresUpgrader(
+            args.pg_target_versions,
+            tags=args.rds_db_instance_tags
+        )
+        rds_postgres_upgrader.upgrade()
+
+        self.assertEqual(describe_dbs_mock.call_count, 1)
+        self.assertEqual(list_tags_mock.call_count, 1)
+        self.assertEqual(modify_db_mock.call_count, 0)
+
 if __name__ == '__main__':
     unittest.main()
