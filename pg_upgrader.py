d8963b35fad256ac1d2aef4c2fe0690398a8ebf9
Scott Ouellette <scottx611x@gmail.com>
2018-09-24 08:40:50 -0400

    Add support for upgrading many rds instances at once

--- pg_upgrader.py
+++ pg_upgrader.py
@@ -7,28 +7,28 @@ import boto3
 class RDSPostgresUpgrader():
     client = boto3.client('rds')
 
-    def __init__(self, db_instance_id, pg_engine_versions):
-        self.db_instance_id = db_instance_id
+    def __init__(self, db_instance_ids, pg_engine_versions):
+        self.db_instance_ids = db_instance_ids
         self.pg_engine_versions = pg_engine_versions
 
-    def _modify_db(self, pg_engine_version):
-        print("Waiting for {} to become available".format(self.db_instance_id))
+    def _modify_db(self, db_instance_id, pg_engine_version):
+        print("Waiting for {} to become available".format(db_instance_id))
         self.client.get_waiter("db_instance_available").wait(
-            DBInstanceIdentifier=self.db_instance_id
+            DBInstanceIdentifier=db_instance_id
         )
         self.client.modify_db_instance(
-            DBInstanceIdentifier=self.db_instance_id,
+            DBInstanceIdentifier=db_instance_id,
             EngineVersion=pg_engine_version,
             AllowMajorVersionUpgrade=True,
             ApplyImmediately=True
         )
-        print("Upgrading {} to: {}".format(
-              self.db_instance_id, pg_engine_version))
+        print("Upgrading {} to: {}".format(db_instance_id, pg_engine_version))
         time.sleep(30)
 
     def upgrade(self):
-        [self._modify_db(pg_engine_version)
-            for pg_engine_version in self.pg_engine_versions]
+        for db_instance_id in self.db_instance_ids:
+            for pg_engine_version in self.pg_engine_versions:
+                self._modify_db(db_instance_id, pg_engine_version)
 
 
 def create_parser():
@@ -36,8 +36,8 @@ def create_parser():
         description='Gather RDSPostgresUpgrader configurables.'
     )
     parser.add_argument(
-        '-id', '--rds_db_instance_id', type=str,
-        help='RDS DBInstanceIdentifier to target for an upgrade',
+        '-id', '--rds_db_instance_ids', type=str, nargs='+',
+        help='RDS DBInstanceIdentifier(s) to target for an upgrade',
         required=True
     )
     parser.add_argument(
@@ -50,7 +50,7 @@ def create_parser():
 
 def main():
     args = create_parser().parse_args()
-    RDSPostgresUpgrader(args.rds_db_instance_id,
+    RDSPostgresUpgrader(args.rds_db_instance_ids,
                         args.pg_target_versions).upgrade()
 
 if __name__ == '__main__':
--- tests.py
+++ tests.py
@@ -6,9 +6,11 @@ from pg_upgrader import RDSPostgresUpgrader, create_parser
 class RDSPostgresUpgraderTests(unittest.TestCase):
     @mock.patch("pg_upgrader.RDSPostgresUpgrader._modify_db")
     def test_upgrade(self, modify_db_mock):
+        db_instance_ids = ["test-rds-id-a", "test-rds-id-b"]
         pg_engine_versions = ["9.4.18", "9.5.13", "9.6.9", "10.4"]
-        RDSPostgresUpgrader("test-rds-id", pg_engine_versions).upgrade()
-        self.assertEqual(modify_db_mock.call_count, len(pg_engine_versions))
+        RDSPostgresUpgrader(db_instance_ids, pg_engine_versions).upgrade()
+        self.assertEqual(modify_db_mock.call_count,
+                         len(pg_engine_versions) * len(db_instance_ids))
 
     @mock.patch("pg_upgrader.RDSPostgresUpgrader._modify_db")
     def test_upgrade_after_parse(self, modify_db_mock):
@@ -16,7 +18,7 @@ class RDSPostgresUpgraderTests(unittest.TestCase):
         args = parser.parse_args(
             ['-id', 'test-rds-id', '-v', "9.4.18"]
         )
-        RDSPostgresUpgrader(args.rds_db_instance_id,
+        RDSPostgresUpgrader(args.rds_db_instance_ids,
                             args.pg_target_versions).upgrade()
         self.assertEqual(modify_db_mock.call_count, 1)
 
